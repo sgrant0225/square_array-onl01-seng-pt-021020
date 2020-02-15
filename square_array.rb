b6bd34a8dfae78089b2554182dd78b11fbfc9092
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-15 19:31:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,21 @@
 require 'pry'
 
-def square_array(array)
+# def square_array(array)
+#   new_array = []
+#   element = 0
+#   array.each do 
+#     new_array << array[element] ** 2
+#     element += 1
+#   end
+#   new_array
+# end
+    ######### OPTION 2 CODE ######
+  def square_array(array)
   new_array = []
-  element = 0
-  array.each do 
-    new_array << array[element] ** 2
-    element += 1
+  array.each do |i|
+    new_array << i ** 2
+    i += 1
   end
   new_array
 end
 
-  
